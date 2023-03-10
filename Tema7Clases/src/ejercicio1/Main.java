909883c60c3be7cb1e9b8ec8a1edc9b32abb28e5
lcardozo <lcardozo@106-25.info2>
2023-03-10 12:42:10 +0100

    jeje

--- Tema7Clases/src/ejercicio1/Main.java
+++ Tema7Clases/src/ejercicio1/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 	//pruebas
+		//Debo crear el objeto y asignarle los valores
 		CuentaCorriente cuenta =  new CuentaCorriente("Y6371330E", "Luisa", 5);
-		
+		 //y luego con el objeto creado llamo a los metodos
 		cuenta.mostrarInformacion();
 
 	}
