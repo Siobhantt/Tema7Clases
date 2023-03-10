196e67455e8fc44c3289ddbe147441c1a7202713
lcardozo <lcardozo@106-25.info2>
2023-03-10 12:41:27 +0100

    Creo que ya entendi un poquito

--- Tema7Clases/src/ejercicio1/Main.java
+++ Tema7Clases/src/ejercicio1/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 	//pruebas
-		CuentaCorriente cuenta =  new CuentaCorriente();
+		CuentaCorriente cuenta =  new CuentaCorriente("Y6371330E", "Luisa", 5);
 		
+		cuenta.mostrarInformacion();
 
 	}
 
